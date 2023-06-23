02252404b98c79dec62407e80291624698bd9a82
ahmedaliichaudhry <chauahme@sheridancollege.ca>
2023-06-23 10:20:03 -0400

    TestUserCommit

---
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ * The user profile for the OnlineVids system. The user has a userID
+ * and a favourite genre that must be one of the choices in the
+ * genres array. This code is to be used for SYST 17796 midterm exam.
+ * Students are reminded to add themselves as modifiers when editing the
+ * code.
+ * @author dancye, 2019
+ */
+public class UserProfile 
+{
+    
+    private String userID;//the userID
+    private String genre;// the user's preferred genre of movie
+    private String[] genres = {"Comedy", "Drama", "Action", "Mystery"};//available genres of movies
+    
+    /**
+     * A constructor that takes in the userID and the favourite genre
+     * @param givenID the ID to assign to this user
+     * @param givenGenre the users favourite genre
+     */
+    public UserProfile(String givenID, String givenGenre)
+    {
+        userID = givenID;
+        genre= givenGenre;
+    }
+
+    /**
+     * A getter for the userID
+     * @return the userID
+     */
+    public String getUserID() {
+        return userID;
+    }
+
+    /**
+     * @param userID the userID to set
+     */
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    /**
+     * A getter for the genre
+     * @return the genre
+     */
+    public String getGenre() {
+        return genre;
+    }
+
+    /**
+     * @param genre the genre to set
+     */
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
+    
+    
+    
+    
+}//end class
+
+import java.util.Scanner;
+
+public class TestUserProfile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Choose your favorite genre from the following options:");
+        String[] genres = {"Comedy", "Drama", "Action", "Mystery"};
+        for (int i = 0; i < genres.length; i++) {
+            System.out.println((i + 1) + ". " + genres[i]);
+        }
+
+        System.out.print("Enter the number corresponding to your favorite genre: ");
+        int choice = scanner.nextInt();
+
+        if (choice >= 1 && choice <= genres.length) {
+            String favoriteGenre = genres[choice - 1];
+            UserProfile userProfile = new UserProfile(name, favoriteGenre);
+            System.out.println("User profile created: " + userProfile.getUserID() +
+                    ", Favorite Genre: " + userProfile.getGenre());
+        }
+    }
+}
+// end class
